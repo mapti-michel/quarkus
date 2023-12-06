e4430c4cd1fe2e517fcba3e15bf11ae300b883d9
Michel Pereira <michel.a.pereira@accenture.com>
2023-12-06 11:29:23 -0300

    Add project course work on the udemy

---
+++ quarkus-user-backend/src/test/java/acc/br/GreetingControllerIT.java
@@ -0,0 +1,8 @@
+package acc.br;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingControllerIT extends GreetingControllerTest {
+    // Execute the same tests but in packaged mode.
+}
